a17a9d94040d7d55360518a8eb18e4646e6619ec
firdevsilgaz <firdevs@entegreyazilim.com.tr>
2022-12-02 11:52:53 +0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,227 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+import SimpleProgressBar from "./component/SimpleProgressBar";
+export default function Home() {
+  const chunkSize = 5 * 1024 * 1024;
+  const [showProgress, setShowProgress] = useState(false);
+  const [counter, setCounter] = useState(1);
+  const [fileToBeUpload, setFileToBeUpload] = useState(null);
+  const [beginingOfTheChunk, setBeginingOfTheChunk] = useState(0);
+  const [endOfTheChunk, setEndOfTheChunk] = useState(chunkSize);
+  const [progress, setProgress] = useState(0);
+  const [fileGuid, setFileGuid] = useState("");
+  const [fileSize, setFileSize] = useState(0);
+  const [chunkCount, setChunkCount] = useState(0);
+  const [ETags, setETags] = useState([]);
+  const [uploadIdd, setUploadId] = useState("");
+  const [token, setToken] = useState("");
+  const [fileName, setFileName] = useState("");
+
+  // const progressInstance = (
+  //   <simpProgBar simpProgBar progress={progress} />
+  // );
+
+  useEffect(() => {
+    if (fileSize > 0) {
+      fileUpload(counter);
+    }
+    console.log("progress", progress);
+  }, [uploadIdd, progress]);
+
+  useEffect(() => {
+    login();
+  }, []);
+
+  const getFileContext = (e) => {
+    resetChunkProperties();
+    const _file = e.target.files[0];
+    setFileSize(_file.size);
+    setFileName(_file.name);
+    console.log("File size: ", _file.size);
+
+    const _totalCount =
+      _file.size % chunkSize === 0
+        ? _file.size / chunkSize
+        : Math.floor(_file.size / chunkSize) + 1; // Total count of chunks will have been upload to finish the file
+    setChunkCount(_totalCount);
+    console.log("Total Count: ", _totalCount);
+    setFileToBeUpload(_file);
+    const _fileID = uuidv4() + "." + _file.name.split(".").pop();
+    setFileGuid(_fileID);
+    console.log("fileNmae", fileName);
+    uploadStarted(_file.name);
+  };
+
+  const blobToBase64 = (blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+
+  const fileUpload = async (counter) => {
+    setCounter(counter + 1);
+    if (counter <= chunkCount) {
+      console.log("start", beginingOfTheChunk, "end", endOfTheChunk);
+      var chunk = fileToBeUpload.slice(beginingOfTheChunk, endOfTheChunk);
+      console.log("chunk", chunk);
+      var content = await blobToBase64(chunk);
+      console.log("content", content);
+      uploadChunk(content);
+    }
+  };
+
+  const login = async () => {
+    try {
+      // const response = await axios.post("http://localhost:5000/user/login", {
+      const response = await axios.post(
+        "http://b3api-env.eba-8dhb66kv.eu-west-1.elasticbeanstalk.com/user/login",
+        {
+          accountId: "6420340315",
+          username: "firdevs@entegreyazilim.com.tr",
+          password: "14533541",
+        }
+      );
+      console.log("response", response);
+      setToken(response.data.token);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const uploadChunk = async (base64_) => {
+    console.log("base64", base64_);
+    var sendString = base64_.split(",")[1];
+    console.log("sendString", sendString);
+    try {
+      const response = await axios.post(
+        // "http://localhost:5000/multipart/upload",
+        "http://b3api-env.eba-8dhb66kv.eu-west-1.elasticbeanstalk.com/multipart/upload",
+        {
+          AccountId: "6420340315",
+          BucketName: "abdullah.example",
+          ObjectName: fileName,
+          Content: sendString,
+          ChunkIndex: counter,
+          ChunkMax: chunkCount,
+          UploadId: uploadIdd,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      const data = response.data;
+      if (data.isSuccess) {
+        console.log("data", data);
+        console.log("ETags1", ETags);
+        var arr = [...ETags]; //copy array by value
+        console.log("arr1", arr);
+        arr.push({ ETag: data.eTag, PartNumber: data.partNumber });
+        setETags(arr);
+        console.log("arr2", arr);
+        console.log("ETags2", ETags);
+
+        setBeginingOfTheChunk(endOfTheChunk);
+        setEndOfTheChunk(endOfTheChunk + chunkSize);
+        if (counter === chunkCount) {
+          console.log("Process is complete, counter", counter);
+
+          await uploadCompleted(arr);
+        } else {
+          var percentage = (counter / chunkCount) * 100;
+          setProgress(percentage);
+        }
+      } else {
+        console.log("Error Occurred:", data.errorMessage);
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const uploadStarted = async (fileName_) => {
+    console.log("token", token);
+    try {
+      axios
+        .post(
+          // "http://localhost:5000/multipart/initiate",
+          "http://b3api-env.eba-8dhb66kv.eu-west-1.elasticbeanstalk.com/multipart/initiate",
+          {
+            AccountId: "6420340315",
+            BucketName: "abdullah.example",
+            ObjectName: fileName_,
+            UploadId: "a1b2c3d4e5f6g7h8i9j0",
+          },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          const data = response.data;
+          setUploadId(data.uploadId);
+          if (data.isSuccess) {
+            setProgress(100);
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const uploadCompleted = async (arr_) => {
+    console.log("eTags", ETags);
+    var formData = new FormData();
+    formData.append("fileName", fileGuid);
+
+    const response = await axios.post(
+      // "http://localhost:5000/multipart/complete",
+      "http://b3api-env.eba-8dhb66kv.eu-west-1.elasticbeanstalk.com/multipart/complete",
+      {
+        AccountId: "6420340315",
+        ObjectName: fileName,
+        BucketName: "abdullah.example",
+        ETags: arr_,
+        UploadId: uploadIdd,
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    const data = response.data;
+    if (data.isSuccess) {
+      setProgress(100);
+    }
+  };
+
+  const resetChunkProperties = () => {
+    setShowProgress(true);
+    setProgress(0);
+    setCounter(1);
+    setBeginingOfTheChunk(0);
+    setEndOfTheChunk(chunkSize);
+    console.log("endOfTheChunk", chunkSize);
+  };
+  return (
+    <div
+      className="justify-content-center align-items-center"
+    >
+      <div style={{ width: '100%' }}>
+        <form action="#" method="post">
+          <div className="form-group mt-3">
+            <label className="mr-2">Upload File</label>
+            <input name="file" type="file" onChange={getFileContext} />
+          </div>
+        </form>
+      </div>
+      <div className="" style={{ width: '50%' }}>
+        <SimpleProgressBar progress={progress} />
+      </div>
+    </div>
+  );
+}
